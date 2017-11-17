c409add980bf9b3fd7186bb55103cdb9f096b1d9
Rob Chandler <robkadt@gmail.com>
2017-11-17 15:09:33 +0000

    Styled metric widget

--- src/Views/Widget/Renderers/Metric.elm
+++ src/Views/Widget/Renderers/Metric.elm
@@ -1,11 +1,25 @@
 module Views.Widget.Renderers.Metric exposing (render)
 
 import Data.Widget as Widget exposing (Widget, Body)
-import Data.Widget.Table as Table exposing (Data, Cell)
+import Data.Widget.Table as Table exposing (Data)
 import Data.Widget.Adapters.Adapter exposing (Adapter(..))
 import Data.Widget.Adapters.MetricAdapter as MetricAdapter
 import Html exposing (..)
-import Html.Attributes exposing (attribute, class, classList, href, id, placeholder, src, title, style)
+import Html.Attributes exposing (class, title)
+
+
+viewMetric : String -> String -> Html msg
+viewMetric label metric =
+    div [ class "card d-inline-block" ]
+        [ div [ class "card-header" ]
+            [ text label
+            ]
+        , div [ class "card-block" ]
+            [ div [ class "" ]
+                [ div [ class "h1 font-weight-bold" ] [ text metric ]
+                ]
+            ]
+        ]
 
 
 render : Int -> Int -> Widget Body -> Table.Data -> Html msg
@@ -17,11 +31,10 @@ render width height widget data =
                     MetricAdapter.adapt widget.metricAdapterConfig data
             in
                 div
-                    [ class <| "col-md-12 widget" ]
+                    [ class "col-md-12 widget container" ]
                     [ h3 [ title widget.description, class "heading" ] [ Html.text widget.name ]
-                    , span [] [ text source ]
-                    , span [] [ text " / " ]
-                    , span [] [ text target ]
+                    , viewMetric "Actual" source
+                    , viewMetric "Target" target
                     ]
 
         _ ->
