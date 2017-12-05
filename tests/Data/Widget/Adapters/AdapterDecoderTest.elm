d1dc52c5a2504287df5c4cc186a8856fbb4fe577
msp <matt@cenatus.org>
2017-12-05 20:22:22 +0000

    Allow specific adapters to decide how to decode their Config settings

--- tests/Data/Widget/Adapters/AdapterDecoderTest.elm
+++ tests/Data/Widget/Adapters/AdapterDecoderTest.elm
@@ -2,8 +2,10 @@ module Data.Widget.Adapters.AdapterDecoderTest exposing (..)
 
 import Expect exposing (Expectation)
 import Data.Widget.Adapters.Adapter as Adapter exposing (Adapter(..))
+import Data.Widget.Adapters.CellPosition as CellPosition exposing (..)
 import Data.Widget.Adapters.MetricAdapter as MetricAdapter exposing (defaultConfig)
 import Json.Decode as Decode exposing (..)
+import Json.Encode as Encode exposing (..)
 import Test exposing (..)
 import Dict exposing (Dict)
 
@@ -41,11 +43,22 @@ adapterDecoderTest =
 
                         }
                         """
+
+                    expectedSourceCell =
+                        CellPosition.asJsonValue ( 1, 1 )
+
+                    expectedTargetCell =
+                        CellPosition.asJsonValue ( 1, 3 )
                 in
                     Expect.equal
                         (Decode.decodeString Adapter.decoder input)
                         (Ok
-                            (Adapter.METRIC <| Dict.fromList [ ( "sourceCell", ( 1, 1 ) ), ( "targetCell", ( 1, 3 ) ) ])
+                            (Adapter.METRIC <|
+                                Dict.fromList
+                                    [ ( "sourceCell", expectedSourceCell )
+                                    , ( "targetCell", expectedTargetCell )
+                                    ]
+                            )
                         )
     in
         Test.describe "Adapter.decode"
